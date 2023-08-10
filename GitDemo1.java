4bcc7c4abe25b5a93b7efaf341d1c219e08426b7
NagarajuJN <nagaraju1998jn@gmail.com>
2023-08-10 18:20:06 +0530

    Added For Loop

--- GitDemo1.java
+++ GitDemo1.java
@@ -2,6 +2,8 @@ class GitDemo1
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Never Giveup");
+		for(int i=0;i<=10;i++){
+			System.out.println("Pentagon Space");
+		}
 	}
 }
